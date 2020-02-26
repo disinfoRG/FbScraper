d6ad3e09a7d16c93014445e39dacb3b7dbfae796
andrea-w-wang <ayw255@nyu.edu>
2020-02-26 14:38:52 -0500

    add scrapy to pipfile

---
+++ sitesAirtable/pipelines.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pugsql
+import os
+import json
+from scrapy.exceptions import DropItem
+from sitesAirtable.items import site_type_mapping
+
+queries = pugsql.module("queries/")
+
+
+def upsert_site(queries, value):
+    site = queries.get_site_by_airtable_id(airtable_id=value["airtable_id"])
+    if site is not None:
+        queries.update_site(site_id=site["site_id"], **value)
+    else:
+        queries.insert_site(value)
+
+
+class SiteItemPipeline(object):
+    def process_item(self, item, spider):
+        if item["type"] not in site_type_mapping.keys():
+            raise DropItem(f"Unknown site type '{item['type']}'")
+        else:
+            item["type"] = site_type_mapping[item["type"]]
+        return item
+
+
+class MySQLPipeline(object):
+    def open_spider(self, spider):
+        queries.connect(os.getenv("DB_URL"))
+
+    def process_item(self, item, spider):
+        upsert_site(
+            queries,
+            {
+                "airtable_id": item["airtable_id"],
+                "is_active": item["is_active"],
+                "name": item["name"],
+                "type": item["type"],
+                "url": item["url"],
+                "config": json.dumps(item["config"]),
+                "site_info": json.dumps(item["site_info"]),
+            },
+        )
+
+        return item
+
+    def close_spider(self, spider):
+        queries.disconnect()
---
+++ sitesAirtable/spiders/updateSites.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from sitesAirtable.items import SiteItem, site_config_fields
+
+
+class UpdatesitesSpider(scrapy.Spider):
+    name = "updateSites"
+    allowed_domains = ["api.airtable.com"]
+
+    def get_request(self, offset=None):
+        req_fields = [
+            "id",
+            "approved",
+            "is_active",
+            "name",
+            "url",
+            "type",
+            "article",
+            "following",
+            "depth",
+            "delay",
+            "ua",
+            "selenium",
+        ]
+        url = (
+            "https://api.airtable.com/v0/appdh2WkMremF0G1L/Sites?"
+            + "&".join([f"fields={f}" for f in req_fields])
+            + "&filterByFormula=approved&view=List"
+            + (f"&offset={offset}" if offset is not None else "")
+        )
+        return scrapy.Request(
+            url,
+            headers={
+                "Authorization": f"Bearer {self.settings.get('AIRTABLE_API_KEY')}"
+            },
+            callback=self.parse,
+        )
+
+    def start_requests(self):
+        yield self.get_request()
+
+    def parse_record(self, record):
+        fields = record["fields"]
+        return SiteItem(
+            {
+                "airtable_id": fields["id"],
+                "approved": fields["approved"],
+                "is_active": "is_active" in fields and fields["is_active"],
+                "name": fields["name"],
+                "url": fields["url"],
+                "type": fields["type"],
+                "config": {k: fields[k] for k in site_config_fields if k in fields},
+                "site_info": {},
+            }
+        )
+
+    def filter_record(self, site):
+        accepted = False
+        accepted_site_types = self.settings.get("SITE_TYPES")
+        if site["type"] in accepted_site_types:
+            accepted = True
+        return accepted
+
+    def parse(self, response):
+        data = json.loads(response.text)
+        for record in data["records"]:
+            site = self.parse_record(record)
+            accepted = self.filter_record(site)
+            if not accepted:
+                continue
+            if not ("approved" in site and site["approved"]):
+                continue
+            yield site
+        if "offset" in data:
+            yield self.get_request(offset=data["offset"])
