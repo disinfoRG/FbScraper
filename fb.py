116cab482d44e38bf7588cb2989bc18ee1faabe3
andrea-w-wang <ayw255@nyu.edu>
2020-04-01 17:08:28 -0400

    add discover to fb.py

--- fb.py
+++ fb.py
@@ -4,6 +4,21 @@ import sys
 import pathlib
 import subprocess
 import argparse
+import pugsql
+import os
+import logging
+import fb_site
+
+db = pugsql.module("queries")
+db.connect(os.getenv("DB_URL"))
+
+logger = logging.getLogger(__name__)
+
+
+def discover():
+    site_ids = db.get_all_site_ids()
+    for entry in site_ids:
+        fb_site.discover(site_id=entry['site_id'])
 
 
 def try_subcommands(skip_commands=[]):
---
+++ queries/get_all_site_ids.sql
@@ -0,0 +1,2 @@
+-- :name get_all_site_ids :many
+SELECT site_id FROM Site;
